5489645bc009321fa8f76b3b1fc7ebf1a514aabc
Smuggler0P <90851567+Smuggler0P@users.noreply.github.com>
2025-05-13 17:21:57 +0530

    Update maintenance_schedule_child.py

--- fstp/fstp/doctype/maintenance_schedule_child/maintenance_schedule_child.py
+++ fstp/fstp/doctype/maintenance_schedule_child/maintenance_schedule_child.py
@@ -4,23 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-
-frappe.ui.form.on('Maintenance Schedule And Actual', {
-    onload: function(frm) {
-        frm.fields_dict.scheduling_table.grid.get_field('issue').get_query = function(doc, cdt, cdn) {
-            let child = locals[cdt][cdn];
-            if (child.maintenance_type === "Breakdown") {
-                return {
-                    filters: {
-                        status: "Open"
-                    }
-                };
-            } else {
-                return {
-                    filters: {}
-                };
-            }
-        };
-    }
-});
-
+class MaintenanceScheduleChild(Document):
+	pass
