e1d896fd598eee75f1a6ea14a8e99dc967e9c4a4
SisodiaPriyamraj <priyamrajsinghsisodia@gmail.com>
2025-05-13 16:29:19 +0530

    Added filter in issue so that only open issues are displayed

--- fstp/fstp/doctype/maintenance_schedule_child/maintenance_schedule_child.py
+++ fstp/fstp/doctype/maintenance_schedule_child/maintenance_schedule_child.py
@@ -5,5 +5,22 @@
 from frappe.model.document import Document
 
 
-class MaintenanceScheduleChild(Document):
-	pass
+frappe.ui.form.on('Maintenance Schedule And Actual', {
+    onload: function(frm) {
+        frm.fields_dict.scheduling_table.grid.get_field('issue').get_query = function(doc, cdt, cdn) {
+            let child = locals[cdt][cdn];
+            if (child.maintenance_type === "Breakdown") {
+                return {
+                    filters: {
+                        status: "Open"
+                    }
+                };
+            } else {
+                return {
+                    filters: {}
+                };
+            }
+        };
+    }
+});
+
