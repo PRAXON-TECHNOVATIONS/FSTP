2b50ea2de6398d918a3cc109184a7b3126f99693
Nahush Singh <nahushsingh7@gmail.com>
2025-06-23 15:21:41 +0530

    feature: final list doctype added

---
+++ fstp/fstp/doctype/final_list_child/final_list_child.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kapil and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class finallistchild(Document):
+	pass
